13d0ceaedae0282921bdffe73a6c1db4daf62223
Kenny Mai <kennymai520@gmail.com>
2020-08-23 04:45:19 -0700

    finalized version

--- js/index.js
+++ js/index.js
@@ -153,8 +153,58 @@ function fetchData2(stateName, typeName, range) {
 // }
 
 
+
 //initialize plot
 fetchData2('Alabama', 'Confirmed', '7');
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (let key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
+  }
+}
+
+let today = '8/18/20';
+function fetchTable(givenDate) {
+  let promise = d3.csv("https://raw.githubusercontent.com/kennymai520/testing/master/14day_cases_count.csv");
+  promise.then((data) => {
+    let latestDay = data.filter((item) => {return item.Date == givenDate;});
+    let cases = [];
+    latestDay.map((item) => {
+      cases.push({
+        Date: item.Date,
+        State: item.Province_State,
+        'Confirmed Cases': item.Confirmed,
+        Deaths: item.Deaths
+      })
+    });
+    let table = document.querySelector("#table_container");
+    let rowName = Object.keys(cases[0]);
+    generateTableHead(table, rowName);
+    generateTable(table, cases);
+  });
+}
+
+// initialize table
+fetchTable(today);
+// hiding the table at first
+document.querySelector('#table_container').style.display = 'none';
+
+
 
 let selectState = document.querySelector('#stateSelector');
 selectState.addEventListener('change', (event) => {
@@ -176,3 +226,15 @@ selectRange.addEventListener('change', (event) => {
   givenRange = event.target.value;
   console.log(givenRange);
 })
+
+let displayTable = document.querySelector('#display_table');
+displayTable.addEventListener('click', () => {
+  document.querySelector('#graph_container').style.display = 'none';
+  document.querySelector('#table_container').style.display = 'table';
+});
+
+let displayGraph = document.querySelector('#display_graph');
+displayGraph.addEventListener('click', () => {
+  document.querySelector('#table_container').style.display = 'none';
+  document.querySelector('#graph_container').style.display = 'block';
+});
