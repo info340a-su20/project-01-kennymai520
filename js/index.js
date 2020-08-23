f8bbc1649aa62cdcb6609420b93c99d44fbbbbe2
Kenny Mai <kennymai520@gmail.com>
2020-08-23 10:13:08 -0700

    final final but js import did not pass jest test

--- js/index.js
+++ js/index.js
@@ -4,23 +4,6 @@ let givenState = 'Alabama';
 let givenRange = '7';
 let givenType = 'Confirmed';
 
-// this version does not work due to the website header doesn't set access-control-allow-origin
-// nothing we can do from client side.
-// let URL3 = "https://covidtracking.com/api/us/daily";
-
-// fetch(URL3)
-// .then(function(response) {
-//   return response.json();
-// }).then (function(data) {
-//   let time = document.querySelector('#case_time');
-//   time.textContent = 'Updated August ' + new Date(data[0].dateChecked);
-//   document.querySelector('#total_cases').textContent = numberWithCommas(data[0].positive);
-//   document.querySelector('#total_death').textContent = numberWithCommas(data[0].death);
-//   console.log(data);
-// }) .catch((err) => {
-//   alert(err);
-// });
-
 // formattting with commas on thousand for numbers.
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -48,8 +31,8 @@ function fetchUS() {
     // document.querySelector('#total_death').textContent = numberWithCommas(data[0].death);
   });
 
-  let promise_death = d3.csv("https://covid.ourworldindata.org/data/ecdc/total_deaths.csv");
-  promise_death.then((data) => {
+  let promiseDeath = d3.csv("https://covid.ourworldindata.org/data/ecdc/total_deaths.csv");
+  promiseDeath.then((data) => {
     return data[data.length - 1];
   }).then((data) => {
     let time = document.querySelector('#case_time');
@@ -57,27 +40,6 @@ function fetchUS() {
     document.querySelector('#total_death').textContent = numberWithCommas(data['United States']);
   });
 }
-
-// version that does not render when page first load 
-// function fetchData(stateName, typeName, range) {
-//   let promise = d3.csv("https://raw.githubusercontent.com/kennymai520/testing/master/14day_cases_count.csv");
-//   promise.then((data) => {
-//     let filterState = data.filter((item) => {return item.Province_State == '' + stateName});
-//     // filter to 7 day or 14 day
-//     let filterRange = filterState.slice(0, parseInt(range));
-//     // extract the date and confirmed cases as x and y stored in array of object.
-//     let dataPoints = [];
-//     filterRange.map((oneDay) => {
-//       let components = {
-//         date: new Date(oneDay.Date),
-//         count: parseInt(oneDay[typeName])
-//       }
-//       dataPoints.push(components);
-//     });
-//     window.onload(dataPoints, givenState);
-//     console.log(dataPoints);
-//   })
-// }
 
 // version two that works fine so far, and simpler code
 function fetchData2(stateName, typeName, range) {
@@ -110,49 +72,6 @@ function fetchData2(stateName, typeName, range) {
     chart.render();
   });
 }
-
-// window.onload = function (inputs, statename) {
-//   let chart = new CanvasJS.Chart("chartContainer", {
-//     animationEnabled: true,
-//     theme: "light2",
-//     title:{
-//       text: "Daily Updates in " + statename
-//     },
-//     data: [{
-//       type: "line",
-//       indexLabelFontSize: 16,
-//       dataPoints: inputs
-//     }]
-//   });
-//   chart.render();
-// }
-
-// window.onload = function (inputs, statename) {
-//   window.document.body.onload = makeGraph;
-//   function makeGraph (inputs, statename) {
-//     let datas = [];
-//     for (let i = 0; i < inputs.length; i++) {
-//       datas.push({x: inputs[i].date, y: inputs[i].count});
-//     }
-//     // console.log(datas);
-//     let chart = new CanvasJS.Chart("chartContainer", {
-//       animationEnabled: true,
-//       theme: "light2",
-//       title:{
-//         text: "Daily Updates in " + statename
-//       },
-//       data: [{        
-//         type: "line",
-//         indexLabelFontSize: 16,
-//         dataPoints: datas
-//       }]
-//     });
-//     console.log("Graph rendered");
-//     chart.render();
-//   }
-// }
-
-
 
 //initialize plot
 fetchData2('Alabama', 'Confirmed', '7');
